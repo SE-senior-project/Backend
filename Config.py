de7137abb2110b6d7f80c4cc12b483b31a0c3631
PasakonPJ <paskon_paingjai@cmu.ac.th>
2022-05-27 14:17:07 +0700

    [dev]: update server in config

--- Config.py
+++ Config.py
@@ -1,9 +1,21 @@
-
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="password",
-    database='onemeasure'
-)
+
+def create_server():
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="password",
+            database='onemeasure'
+        )
+        print('MySQL Database connection successful')
+    except:
+        pass
+    return db
+
+
+builder = create_server()
+
+def build_table():
+    cursor = builder.cursor()
